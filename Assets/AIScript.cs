01345b3baba47e208f87fa7196868ede2b5020cd
Domniki Barbari <dombarbari@gmail.com>
2025-11-04 13:34:03 +0200

    Nav Mesh trial

--- Assets/AIScript.cs
+++ Assets/AIScript.cs
@@ -1,16 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AIScript : MonoBehaviour
 {
+    private NavMeshAgent agent;
+    private Transform Player;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        Player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        agent.SetDestination(Player.position);
     }
 }
