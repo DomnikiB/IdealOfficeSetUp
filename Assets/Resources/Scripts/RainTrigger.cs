d1d0c95f25a080a2e1c9de24dae5811205b2f110
Domniki Barbari <dombarbari@gmail.com>
2025-11-11 16:09:25 +0200

    rain eventTrigger tagFilter

--- Assets/Resources/Scripts/RainTrigger.cs
+++ Assets/Resources/Scripts/RainTrigger.cs
@@ -1,17 +1,22 @@
+using System;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class RainTrigger : MonoBehaviour
 {
+    [SerializeField] string tagFilter; //checks player's tag 
     [SerializeField] UnityEvent onTriggerEnter;
     [SerializeField] UnityEvent onTriggerExit;
+    
     void OnTriggerEnter(Collider other)
     {
+    	if (!String.IsNullOrEmpty(tagFilter) && !other.gameObject.CompareTag(tagFilter)) return;
     	onTriggerEnter.Invoke(); 
     }
     
     void OnTriggerExit(Collider other)
     {
+    	if (!String.IsNullOrEmpty(tagFilter) && !other.gameObject.CompareTag(tagFilter)) return;
     	onTriggerExit.Invoke(); 
     }
 }
