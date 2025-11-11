c12c1a4f2327dc2ef1ff1ba4a7a3a34518f79c0a
Domniki Barbari <dombarbari@gmail.com>
2025-11-11 15:52:19 +0200

    rain eventTrigger

---
+++ Assets/Resources/Scripts/RainTrigger.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class RainTrigger : MonoBehaviour
+{
+    [SerializeField] UnityEvent onTriggerEnter;
+    [SerializeField] UnityEvent onTriggerExit;
+    void OnTriggerEnter(Collider other)
+    {
+    	onTriggerEnter.Invoke(); 
+    }
+    
+    void OnTriggerExit(Collider other)
+    {
+    	onTriggerExit.Invoke(); 
+    }
+}
