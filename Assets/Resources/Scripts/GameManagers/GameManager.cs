30db605b7ff50ecd35254940f31fb56356720e9f
Domniki Barbari <dombarbari@gmail.com>
2025-11-21 13:17:16 +0200

    UI stuff

---
+++ Assets/Resources/Scripts/GameManagers/GameManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{    
+    public void QuitApp() 
+    {
+    	Application.Quit();
+    }
+    
+    public void PauseGame()
+    {
+    	Time.timeScale = 0;
+    }
+    
+    public void ResumeGame()
+    {
+    	Time.timeScale = 1;
+    }
+    
+}
